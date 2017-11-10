b0280882fd4e37ea71e4c45c6b97628e58548a34
devsdmf <devsdmf@gmail.com>
2017-11-10 14:31:13 -0200

    added extension hooks for module init/shutdown, request init/shutdown and module info

--- hello.c
+++ hello.c
@@ -12,15 +12,48 @@ const zend_function_entry hello_functions[] = {
     PHP_FE_END
 };
 
+PHP_MINIT_FUNCTION(hello)
+{
+    /* Here the module is being initialized */
+    return SUCCESS;
+}
+
+PHP_RSHUTDOWN_FUNCTION(hello)
+{
+    /* Here the module is deinitialized */
+    return SUCCESS;
+}
+
+PHP_RINIT_FUNCTION(hello)
+{
+#if defined(ZTS) && defined(COMPILE_DL_HELLO)
+    ZEND_TSRMLS_CACHE_UPDATE();
+#endif
+
+    return SUCCESS;
+}
+
+PHP_RSHUTDOWN_FUNCTION(hello)
+{
+    return SUCCESS;
+}
+
+PHP_MINFO_FUNCTION(hello)
+{
+    php_info_print_table_start();
+    php_info_print_table_header(2, "hello support", "enabled");
+    php_info_print_table_end();
+}
+
 zend_module_entry hello_module_entry = {
     STANDARD_MODULE_HEADER,
     "hello", /* Extension name */
     hello_functions, /* zend_function_entry */
-    NULL, /* PHP_MINIT - Module initialization */
-    NULL, /* PHP_MSHUTDOWN - Module shutdown */
-    NULL, /* PHP_RINIT - Request initialization */
-    NULL, /* PHP_RSHUTDOWN - Request shutdown */
-    NULL, /* PHP_MINFO - Module info */
+    PHP_MINIT(hello), /* PHP_MINIT - Module initialization */
+    PHP_MSHUTDOWN(hello), /* PHP_MSHUTDOWN - Module shutdown */
+    PHP_RINIT(hello), /* PHP_RINIT - Request initialization */
+    PHP_RSHUTDOWN(helo), /* PHP_RSHUTDOWN - Request shutdown */
+    PHP_MINFO(hello), /* PHP_MINFO - Module info */
     PHP_HELLO_VERSION, /* Version */
     STANDARD_MODULE_PROPERTIES
 };
